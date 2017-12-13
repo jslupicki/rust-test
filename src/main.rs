2b55c825e260af97948e96c686d0d876501b7b37
jslupicki <jan@slupicki.com>
2017-12-13 08:15:44 +0100

    Add .gitignore and some clean up

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,6 @@
+mod start;
 mod test;
 
 fn main() {
-
-    println!("Witaj Świecie!");
-
-    let letters = vec!["a", "b", "c"];
-
-    println!("letters: {:?}", letters);
-
-    for l in &letters {
-        println!("{}", l)
-    }
-
-    for i in 0..letters.len() {
-        println!("letters[{}] = {}", i, letters[i]);
-    }
-
-    println!("Żegnaj Świecie");
-    test::print("Hello");
-    test::print("World");
-    test::print("www");
-}
-
-#[cfg(test)]
-mod tests {
-    
-    #[test]
-    fn it_works() {
-        println!("Unit test:it_works")
-    }
+    start::start();
 }
